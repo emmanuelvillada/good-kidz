20d9f9e7dbfcc45d711fb7643d009be0e0e47b95
Emmanuel <emmanuelvillada1903@gmail.com>
2024-05-06 19:56:08 -0500

    making footer

--- good-kidz/src/App.js
+++ good-kidz/src/App.js
@@ -1,11 +1,15 @@
 import logo from './good kidz.jpg';
 import './App.css';
 import Header from './components/Header.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
   <Header logo = { logo }
   ></Header>
+    <Footer>
+      
+    </Footer>
   );
 }
 
---
+++ good-kidz/src/components/Footer.js
@@ -0,0 +1,20 @@
+import './Footer.css';
+
+function Footer(props) {
+
+    return (
+        <footer>
+            <div>
+                <p>Copyright &copy; 2021</p>
+                <p>Good Kidz</p>
+                <p>All Rights Reserved</p>
+                <p>Privacy Policy</p>
+                <p>Terms of Use</p>
+                <a href = "www.instagram.com"><p>Contact Us</p></a>
+            </div>
+
+        </footer
+    );
+}
+
+export default Footer;
