b4afd4c95b7626d1e3f2aa58ea532c17657c3cbc
Emmanuel Villada Suarez <emmanuelvillada310016@correo.itm.edu.co>
2024-07-18 23:02:15 -0500

    añadiendo tema

--- good-kidz/src/components/Header.jsx
+++ good-kidz/src/components/Header.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { PaletteIcon } from '../pages/icons.jsx';
+import ThemeToggle from './ThemeToggle.jsx';
+
 function Header() {
     return (
-    <header className="px-4 lg:px-6 h-18 flex items-center">
-                <Link to="/" className="flex items-center justify-center">
-                    <PaletteIcon className="h-6 w-6" />
-                    <span className="text-2xl font-bold">Good Kidz</span>
+        <header className="px-4 lg:px-6 h-18 flex items-center">
+            <Link to="/" className="flex items-center justify-center">
+                <PaletteIcon className="h-6 w-6" />
+                <span className="text-2xl font-bold">Good Kidz</span>
+            </Link>
+            <nav className="ml-auto flex gap-4 sm:gap-6">
+            <ThemeToggle /> 
+                <Link to="/events" className="text-md font-medium hover:underline underline-offset-4">
+                    Events
                 </Link>
-                <nav className="ml-auto flex gap-4 sm:gap-6">
-                    <Link to="/events" className="text-md font-medium hover:underline underline-offset-4">
-                        Events
-                    </Link>
-                    <Link to="/about" className="text-md font-medium hover:underline underline-offset-4">
-                        Gallery
-                    </Link>
-                    <Link to="/about" className="text-md font-medium hover:underline underline-offset-4">
-                        About
-                    </Link>
-                    <Link to="/create-event" className="text-md font-medium hover:underline underline-offset-4">
-                        Contact
-                    </Link>
-                </nav>
-            </header>
+                <Link to="/about" className="text-md font-medium hover:underline underline-offset-4">
+                    Gallery
+                </Link>
+                <Link to="/about" className="text-md font-medium hover:underline underline-offset-4">
+                    About
+                </Link>
+                <Link to="/create-event" className="text-md font-medium hover:underline underline-offset-4">
+                    Contact
+                </Link>
+            </nav>
+        </header>
     );
 }
 
